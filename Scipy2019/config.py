b9c709823e26f5b20d3732e1788b871cdd961a03
prashantsinalkar <prashantsinalkar@gmail.com>
2019-08-27 12:29:12 +0530

    added intial code for project

---
+++ Scipy2019/config.py
@@ -0,0 +1,39 @@
+DB_NAME_DEFAULT = 'SciPy2019'
+DB_USER_DEFAULT = 'root'
+DB_PASS_DEFAULT = 'root'
+DB_HOST_DEFAULT = 'localhost'
+DB_PORT_DEFAULT = ''
+########################################
+GOOGLE_KEY = 'GOOGLE_KEY'
+GOOGLE_SECRET = 'GOOGLE_SECRET'
+FACEBOOK_KEY = 'FACEBOOK_KEY'
+FACEBOOK_SECRET = 'FACEBOOK_SECRET'
+GITHUB_KEY = 'GITHUB_KEY'
+GITHUB_SECRET = 'GITHUB_SECRET'
+RECAPTCHA_PUBLIC = 'RECAPTCHA_PUBLIC'
+RECAPTCHA_PRIVATE = 'RECAPTCHA_PRIVATE'
+SYSADMINS = [('SYSADMINS')]
+SYS_SERVER_EMAIL = 'SYS_SERVER_EMAIL'
+ADMIN_EMAIL_ID = 'admin email'
+########################################
+TO_EMAIL = 'TO_EMAIL'
+CC_EMAIL = 'CC_EMAIL'
+BCC_EMAIL = 'BCC_EMAIL'
+SENDER_EMAIL = 'email@email.com'
+########################################
+# Host for sending e-mail.
+EMAIL_HOST_SERVER = 'localhost'
+# Port for sending e-mail.
+EMAIL_PORT_SERVER = 25
+#########################################
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER_SERVER = ''
+EMAIL_HOST_PASSWORD_SERVER = ''
+EMAIL_USE_TLS_SERVER = False
+#########################################
+ALLOWED_HOSTS_VAL = ['127.0.0.1','localhost']
+DEBUG_VAL = True
+SITE = 'SITE'
+PRODUCTION_URL_NAME = 'Production url'
+ROOT_URL_VAL = 'http://127.0.0.1:8000' #http://127.0.0.1:8000
+#########################################
