d84267b8555a09e1500f839835439142d6c6fc95
Sashi20 <gayatrirekha204@gmail.com>
2019-09-09 15:40:24 +0530

    Update keynote speaker details, poster

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CFP, RegistrationDetail
+from .models import CFP, RegistrationDetail, Proposal
 
 class CFPAdmin(admin.ModelAdmin):
 	list_display = ['start_date','end_date','date_of_announcement']
@@ -7,5 +7,9 @@ class CFPAdmin(admin.ModelAdmin):
 class RegistrationDetailAdmin(admin.ModelAdmin):
 	list_display = ['registration_type', 'start_date', 'end_date', 'registration_ticket','registration_description']		
 
+class ProposalAdmin(admin.ModelAdmin):
+	list_display = ['title', 'email', 'phone']
+
 admin.site.register(CFP, CFPAdmin)
 admin.site.register(RegistrationDetail, RegistrationDetailAdmin)
+admin.site.register(Proposal, ProposalAdmin)
